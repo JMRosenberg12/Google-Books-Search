e679420d9a0b5dc57d3b1e2593f542f839703544
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-23 10:24:16 -0500

    delete google js

--- routes/api/google.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const googleController = require("../../controllers/googlecontroller");
-
-// Matches with "/api/google"
-router.route("/").get(googleController.findAll);
-
-module.exports = router;
