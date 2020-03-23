6fa2c54200edb1386c9b77bb0c20cb0dce9a8612
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-23 09:50:17 -0500

    editing the google require

--- routes/api/google.js
+++ routes/api/google.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const googleController = require("../../controllers/googleController");
+const googleController = require("../../controllers/googlecontroller");
 
 // Matches with "/api/google"
 router.route("/").get(googleController.findAll);
