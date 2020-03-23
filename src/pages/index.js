309f194d050cee561156c5062ec97660039d84b1
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-23 09:22:20 -0500

    editing the index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-// import TextField from '@material-ui/core/TextField';
 
 
 const App = () => {
