2f0cc33f966c7bd0f2f33543fceef02179c4bd80
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-23 08:41:33 -0500

    create a controller folder and bookscontroller.js

---
+++ server/controllers/bookscontroller.js
@@ -0,0 +1,40 @@
+const db = require("../models");
+
+// make sure the defining methods is used for the bookController
+module.exports = {
+    findAll: function (req, res) {
+
+        db.Book.find(req.query)
+            .then((dbBook) => res.json(dbBook))
+            .catch(err => res.status(500).json(err));
+    },
+    findById: function (req, res) {
+
+        const { id } = req.params;
+
+        db.Book.findById(id)
+            .then((dbBook) => res.json(dbBook))
+            .catch(err => res.status(500).json(err));
+    },
+    create: function (req, res) {
+
+        db.Book.create(req.body)
+            .then((dbBook) => res.json(dbBook))
+            .catch(err => res.status(500).json(err));
+    },
+    update: function (req, res) {
+
+        const { id } = req.params;
+        db.Book.findOneAndUpdate({ id: id }, req.body)
+            .then((dbBook) => res.json(dbBook))
+            .catch(err => res.status(500).json(err));
+    },
+    remove: function (req, res) {
+
+        const { id } = req.params;
+        db.Book.findById(id)
+            .then((dbBook) => dbBook.remove())
+            .then((dbBook) => res.json(dbBook))
+            .catch(err => res.status(500).json(err));
+    }
+};
