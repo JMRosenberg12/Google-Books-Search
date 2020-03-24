6c16aeceac87b80c73c771e53a61cbb2fae65706
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-24 15:55:20 -0500

    editing the require

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,17 +1,17 @@
   
 const router = require("express").Router();
-const booksController = require("../../controllers/bookscontroller");
+const bookscontroller = require("../../controllers/bookscontroller");
 
 // Matches with "/api/books"
 router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+  .get(bookscontroller.findAll)
+  .post(bookscontroller.create);
 
 // Matches with "/api/books/:id"
 router
   .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+  .get(bookscontroller.findById)
+  .put(bookscontroller.update)
+  .delete(bookscontroller.remove);
 
 module.exports = router;
