d23afa35c547f781b17528049afb0180766e13a4
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-24 20:24:42 -0500

    redo

--- models/book.js
+++ models/book.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  googleId: { type: String, unique:true },
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: String,
-  image: { type: String },
-  link: { type: String },
+  synopsis: String,
   date: { type: Date, default: Date.now }
 });
 
