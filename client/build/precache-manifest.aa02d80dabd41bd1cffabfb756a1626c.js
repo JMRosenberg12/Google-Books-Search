d9f6844be29cbcacd69521bc2c3bea33a446f75e
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-24 21:02:28 -0500

    build

---
+++ client/build/precache-manifest.aa02d80dabd41bd1cffabfb756a1626c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295c71a27abeb6b1f36939ad1ee71bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac517489343ad4a37364",
+    "url": "/static/css/main.26e067c4.chunk.css"
+  },
+  {
+    "revision": "a4bd6824258b69bf444e",
+    "url": "/static/js/2.3a837e47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3a837e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac517489343ad4a37364",
+    "url": "/static/js/main.840d69b5.chunk.js"
+  },
+  {
+    "revision": "b46e60ab100976f9a945",
+    "url": "/static/js/runtime-main.0dc0fcea.js"
+  }
+]);
