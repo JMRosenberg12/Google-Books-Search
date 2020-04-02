3db40a183e02ad7c9b60eeb5f527763d1108f5cf
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-04-02 16:54:13 -0500

    edit book.js

--- models/book.js
+++ models/book.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  googleId: { type: String, unique:true },
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: [{ type: String, required: true }],
   description: String,
-  image: { type: String },
-  link: { type: String },
+  image: { type: String, trim: true },
+  link: { type: String, trim: true },
   date: { type: Date, default: Date.now }
 });
 
